e27bac4a67dd1d07490efbfed16cd5cb31b76824
Will Eddins <eddinsw@gmail.com>
2011-03-28 23:16:34 -0400

    Updated version to 0.1.1 in assembly

--- starboard-sc2/Properties/AssemblyInfo.cs
+++ starboard-sc2/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
