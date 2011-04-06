fb8782ef91a41a680a0fec54e75814f36479c455
Will Eddins <eddinsw@gmail.com>
2011-04-06 19:50:11 -0400

    Updated version to 0.2.*, tagging version v0.2

--- starboard-sc2/Properties/AssemblyInfo.cs
+++ starboard-sc2/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
