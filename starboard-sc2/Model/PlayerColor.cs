fe7efe13e6dd68cf104f9cf3cb83860f8224f94d
Will Eddins <eddinsw@gmail.com>
2011-04-14 20:13:07 -0400

    Documentation updates

--- starboard-sc2/Model/PlayerColor.cs
+++ starboard-sc2/Model/PlayerColor.cs
@@ -1,15 +1,42 @@
-﻿namespace Starboard.Model
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayerColor.cs" company="Starboard">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Defines the PlayerColor type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starboard.Model
 {
+    /// <summary> Defines the valid colors available to a player in a 1v1 matchup. </summary>
     public enum PlayerColor
     {
+        /// <summary> The player color is unknown. </summary>
         Unknown,
+
+        /// <summary> The color Red. </summary>
         Red,
+
+        /// <summary> The color Blue. </summary>
         Blue,
+
+        /// <summary> The color Teal. </summary>
         Teal,
+
+        /// <summary> The color Purple. </summary>
         Purple,
+
+        /// <summary> The color Yellow. </summary>
         Yellow,
+
+        /// <summary> The color Orange. </summary>
         Orange,
+        
+        /// <summary> The color Green. </summary>
         Green,
+
+        /// <summary> The color Light Pink. </summary>
         LightPink
     }
 }
