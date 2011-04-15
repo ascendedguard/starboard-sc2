dfc3c94858d00e053848b5db05151f1092ba2b14
Will Eddins <eddinsw@gmail.com>
2011-04-14 20:03:14 -0400

    Documentation updates

--- starboard-sc2/App.xaml.cs
+++ starboard-sc2/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="App.xaml.cs" company="Starboard">
-//   Copyright 2011
+//   Copyright © 2011 All Rights Reserved
 // </copyright>
 // <summary>
 //   Interaction logic for App.xaml
