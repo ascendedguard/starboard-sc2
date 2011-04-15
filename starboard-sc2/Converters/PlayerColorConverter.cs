d1d71c64cff4d2aee223873b626e5bf3da425ceb
Will Eddins <eddinsw@gmail.com>
2011-04-14 20:25:25 -0400

    Documentation updates

--- starboard-sc2/Converters/PlayerColorConverter.cs
+++ starboard-sc2/Converters/PlayerColorConverter.cs
@@ -1,4 +1,13 @@
-﻿namespace Starboard
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayerColorConverter.cs" company="Starboard">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Defines the PlayerColorConverter type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starboard
 {
     using System;
     using System.Globalization;
@@ -7,8 +16,15 @@
 
     using Starboard.Model;
 
+    /// <summary> Converts a PlayerColor enumeration into an actual Color for used in overlays. </summary>
     public class PlayerColorConverter : IValueConverter
     {
+        /// <summary> Converts a PlayerColor enumeration into an actual color. </summary>
+        /// <param name="value"> The value. </param>
+        /// <param name="targetType"> The target type. If Brush, a SolidColorBrush is returned. </param>
+        /// <param name="parameter"> The parameter, not used. </param>
+        /// <param name="culture"> The culture. </param>
+        /// <returns> The player color as a System.Windows.Media.Color. If a Brush is requested, a SolidColorBrush is returned. </returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var color = (PlayerColor)value;
@@ -52,6 +68,13 @@
             return selectedColor;
         }
 
+        /// <summary> Not implemented. </summary>
+        /// <param name="value"> The value. </param>
+        /// <param name="targetType"> The target type. </param>
+        /// <param name="parameter"> The parameter. </param>
+        /// <param name="culture"> The culture. </param>
+        /// <returns> Throws a NotImplementedException. </returns>
+        /// <exception cref="NotImplementedException"> Always thrown, as the function is never used. </exception>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
