5a6466c0333407a88aed09b81a448f944234fe95
Will Eddins <eddinsw@gmail.com>
2011-04-30 03:06:02 -0400

    Seperated the player editor view into a seperate usercontrol

---
+++ starboard-sc2/View/PlayerView.xaml.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayerView.xaml.cs" company="Starboard">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Interaction logic for PlayerView.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starboard.View
+{
+    using System.Windows;
+
+    using Starboard.Model;
+
+    /// <summary>
+    /// Interaction logic for PlayerView.xaml
+    /// </summary>
+    public partial class PlayerView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerView"/> class.
+        /// </summary>
+        public PlayerView()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> Increment's the player's score by 1. </summary>
+        /// <param name="sender"> The sender. </param>
+        /// <param name="e"> The event arguments. </param>
+        private void IncrementPlayerScore(object sender, RoutedEventArgs e)
+        {
+            var player = this.DataContext as Player;
+            
+            if (player != null)
+            {
+                player.Score++;                
+            }
+        }
+    }
+}
