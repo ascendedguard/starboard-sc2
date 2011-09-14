ff381d3b3128f5bb0ff86fc9072ae88a71fe5ba7
Will Eddins <eddinsw@gmail.com>
2011-09-14 02:29:31 -0400

    Reworked board to use ContentControl and bind the view and viewmodel through App.xaml; This should lead to an easier transition to a skinable interface

--- starboard-sc2/View/ScoreboardDisplay.xaml.cs
+++ starboard-sc2/View/ScoreboardDisplay.xaml.cs
@@ -3,7 +3,7 @@
 //   Copyright © 2011 All Rights Reserved
 // </copyright>
 // <summary>
-//   View for the scoreboard.
+//   Interaction logic for ScoreboardDisplay.xaml
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -24,19 +24,27 @@ namespace Starboard.View
     /// </summary>
     public partial class ScoreboardDisplay
     {
+        // Using a DependencyProperty as the backing store for Scoreboard.  This enables animation, styling, binding, etc...
         #region Constants and Fields
 
-        /// <summary> The opacity used by the scoreboard when visible. The maximum value used during transitions. </summary>
-        private double maxOpacity = 1;
+        /// <summary>
+        /// The scoreboard property.
+        /// </summary>
+        public static readonly DependencyProperty ScoreboardProperty = DependencyProperty.Register(
+            "Scoreboard", typeof(ScoreboardControlViewModel), typeof(ScoreboardDisplay), new UIPropertyMetadata(null));
 
-        /// <summary> ViewModel for the scoreboard. </summary>
-        private ScoreboardControlViewModel scoreboard;
+        /// <summary>
+        ///   The opacity used by the scoreboard when visible. The maximum value used during transitions.
+        /// </summary>
+        private double maxOpacity = 1;
 
         #endregion
 
         #region Constructors and Destructors
 
-        /// <summary> Initializes a new instance of the <see cref = "ScoreboardDisplay" /> class. </summary>
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "ScoreboardDisplay" /> class.
+        /// </summary>
         public ScoreboardDisplay()
         {
             this.InitializeComponent();
@@ -51,13 +59,19 @@ namespace Starboard.View
 
         #region Public Properties
 
-        /// <summary> Gets or sets a value indicating whether the window should reset positions when first loaded. </summary>
+        /// <summary>
+        ///   Gets or sets a value indicating whether the window should reset positions when first loaded.
+        /// </summary>
         public bool InitializePositionOnLoad { get; set; }
 
-        /// <summary> Gets or sets a value indicating whether the window can be dragged. </summary>
+        /// <summary>
+        ///   Gets or sets a value indicating whether the window can be dragged.
+        /// </summary>
         public bool IsWindowMovable { get; set; }
 
-        /// <summary> Gets or sets the maximum opacity used by the scoreboard. </summary>
+        /// <summary>
+        ///   Gets or sets the maximum opacity used by the scoreboard.
+        /// </summary>
         public double MaxOpacity
         {
             get
@@ -68,16 +82,34 @@ namespace Starboard.View
             set
             {
                 this.maxOpacity = value;
-                if (this.scoreboardControl.IsVisible)
+                if (this.IsVisible)
                 {
                     // Applying the opacity without an animation results in no change. Is there a better way?
                     var animation = new DoubleAnimation(value, new Duration(new TimeSpan(0, 0, 0, 0)));
-                    this.scoreboardControl.BeginAnimation(OpacityProperty, animation);
+                    this.BeginAnimation(OpacityProperty, animation);
                 }
             }
         }
 
-        /// <summary> Gets or sets the width of the viewbox used to render the scoreboard. </summary>
+        /// <summary>
+        /// Gets or sets Scoreboard.
+        /// </summary>
+        public ScoreboardControlViewModel Scoreboard
+        {
+            get
+            {
+                return (ScoreboardControlViewModel)this.GetValue(ScoreboardProperty);
+            }
+
+            set
+            {
+                this.SetValue(ScoreboardProperty, value);
+            }
+        }
+
+        /// <summary>
+        ///   Gets or sets the width of the viewbox used to render the scoreboard.
+        /// </summary>
         public double ViewboxWidth
         {
             get
@@ -95,7 +127,9 @@ namespace Starboard.View
 
         #region Public Methods
 
-        /// <summary> Overrides the original Hide function to support fading in cases where transparency is used. </summary>
+        /// <summary>
+        /// Overrides the original Hide function to support fading in cases where transparency is used.
+        /// </summary>
         public new void Hide()
         {
             if (this.AllowsTransparency)
@@ -103,7 +137,7 @@ namespace Starboard.View
                 var animation = new DoubleAnimation(0, new Duration(new TimeSpan(0, 0, 0, 0, 500)));
                 Action hideAction = base.Hide;
                 animation.Completed += (sender, e) => hideAction();
-                this.scoreboardControl.BeginAnimation(OpacityProperty, animation);
+                this.BeginAnimation(OpacityProperty, animation);
             }
             else
             {
@@ -111,7 +145,9 @@ namespace Starboard.View
             }
         }
 
-        /// <summary> Resets the position of the window to the default location, centered on the primary monitor with a 10px offset from top. </summary>
+        /// <summary>
+        /// Resets the position of the window to the default location, centered on the primary monitor with a 10px offset from top.
+        /// </summary>
         public void ResetPosition()
         {
             if (this.IsMeasureValid == false)
@@ -128,20 +164,25 @@ namespace Starboard.View
             this.Top = 10.0;
         }
 
-        /// <summary> Sets the viewmodel for the window to another instance of ScoreboardControlViewModel </summary>
-        /// <param name="vm"> The viewModel to apply.  </param>
+        /// <summary>
+        /// Sets the viewmodel for the window to another instance of ScoreboardControlViewModel
+        /// </summary>
+        /// <param name="vm">
+        /// The viewModel to apply.  
+        /// </param>
         public void SetViewModel(ScoreboardControlViewModel vm)
         {
-            this.scoreboardControl.DataContext = vm;
-            this.scoreboard = vm;
+            this.Scoreboard = vm;
         }
 
-        /// <summary> Overrides the original Show function to support a fade-in effect in cases where transparency is used. </summary>
+        /// <summary>
+        /// Overrides the original Show function to support a fade-in effect in cases where transparency is used.
+        /// </summary>
         public new void Show()
         {
             if (this.AllowsTransparency)
             {
-                this.scoreboardControl.Opacity = 0;
+                this.Opacity = 0;
             }
 
             base.Show();
@@ -149,7 +190,7 @@ namespace Starboard.View
             if (this.AllowsTransparency)
             {
                 var animation = new DoubleAnimation(this.MaxOpacity, new Duration(new TimeSpan(0, 0, 0, 0, 500)));
-                this.scoreboardControl.BeginAnimation(OpacityProperty, animation);
+                this.BeginAnimation(OpacityProperty, animation);
             }
         }
 
@@ -157,8 +198,12 @@ namespace Starboard.View
 
         #region Methods
 
-        /// <summary> Overrides the OnKeyDown event to handled hotkeys for this window. </summary>
-        /// <param name="e"> The event arguments.  </param>
+        /// <summary>
+        /// Overrides the OnKeyDown event to handled hotkeys for this window.
+        /// </summary>
+        /// <param name="e">
+        /// The event arguments.  
+        /// </param>
         protected override void OnKeyDown(KeyEventArgs e)
         {
             if (e.Key == Key.F1)
@@ -174,20 +219,23 @@ namespace Starboard.View
 
             if (e.KeyboardDevice.Modifiers == ModifierKeys.Control)
             {
-                var result = ApplyHotkey(e.Key, this.scoreboard.Player1);
+                var result = ApplyHotkey(e.Key, this.Scoreboard.Player1);
                 e.Handled = result;
             }
 
-            if (e.KeyboardDevice.Modifiers == ModifierKeys.Alt
-                && e.SystemKey != Key.LeftAlt)
+            if (e.KeyboardDevice.Modifiers == ModifierKeys.Alt && e.SystemKey != Key.LeftAlt)
             {
-                var result = ApplyHotkey(e.SystemKey, this.scoreboard.Player2);
+                var result = ApplyHotkey(e.SystemKey, this.Scoreboard.Player2);
                 e.Handled = result;
             }
         }
 
-        /// <summary> Allows the window to be dragged if IsWindowMovable has been set. </summary>
-        /// <param name="e"> The event arguments.  </param>
+        /// <summary>
+        /// Allows the window to be dragged if IsWindowMovable has been set.
+        /// </summary>
+        /// <param name="e">
+        /// The event arguments.  
+        /// </param>
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
@@ -198,10 +246,18 @@ namespace Starboard.View
             }
         }
 
-        /// <summary> Applies the hotkey for the attached key to the attached player. </summary>
-        /// <param name="key"> The key which was pressed in the hotkey sequence.  </param>
-        /// <param name="player"> The player to apply the change to.  </param>
-        /// <returns> Whether the key pressed was a valid hotkey, and was applied to the player.  </returns>
+        /// <summary>
+        /// Applies the hotkey for the attached key to the attached player.
+        /// </summary>
+        /// <param name="key">
+        /// The key which was pressed in the hotkey sequence.  
+        /// </param>
+        /// <param name="player">
+        /// The player to apply the change to.  
+        /// </param>
+        /// <returns>
+        /// Whether the key pressed was a valid hotkey, and was applied to the player.  
+        /// </returns>
         private static bool ApplyHotkey(Key key, Player player)
         {
             var handled = true;
@@ -268,8 +324,12 @@ namespace Starboard.View
             return handled;
         }
 
-        /// <summary> Creates a text field for typing the player name directly into the scoreboard. Clears the player name upon the hotkey press. </summary>
-        /// <param name="binding"> The binding to apply to the TextBox.  </param>
+        /// <summary>
+        /// Creates a text field for typing the player name directly into the scoreboard. Clears the player name upon the hotkey press.
+        /// </summary>
+        /// <param name="binding">
+        /// The binding to apply to the TextBox.  
+        /// </param>
         private void CreatePlayerChangeField(string binding)
         {
             var field = new TextBox();
@@ -277,7 +337,7 @@ namespace Starboard.View
                 TextBox.TextProperty, 
                 new Binding(binding)
                     {
-                       Source = this.scoreboard, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged 
+                       Source = this.Scoreboard, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged 
                     });
             field.Width = 50;
             field.Height = 20;
@@ -285,8 +345,7 @@ namespace Starboard.View
             field.LostFocus += (sender, e) => this.rootGrid.Children.Remove(field);
             field.KeyDown += (sender, e) =>
                 {
-                    if (e.Key == Key.Enter || e.Key == Key.Return
-                        || e.Key == Key.Escape)
+                    if (e.Key == Key.Enter || e.Key == Key.Return || e.Key == Key.Escape)
                     {
                         this.rootGrid.Children.Remove(field);
                     }
@@ -298,9 +357,15 @@ namespace Starboard.View
             field.Text = string.Empty;
         }
 
-        /// <summary> Resets the position of the window after it has completed loading. </summary>
-        /// <param name="sender"> The sender. </param>
-        /// <param name="e"> The event parameters. </param>
+        /// <summary>
+        /// Resets the position of the window after it has completed loading.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender. 
+        /// </param>
+        /// <param name="e">
+        /// The event parameters. 
+        /// </param>
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
             if (this.InitializePositionOnLoad)
