4c1141b41aee53c89844a3c1ccf5b702af7f0ca4
Will Eddins <eddinsw@gmail.com>
2011-09-14 01:22:49 -0400

    Updated documentation to be more consistent

--- starboard-sc2/ViewModel/ScoreboardControlViewModel.cs
+++ starboard-sc2/ViewModel/ScoreboardControlViewModel.cs
@@ -14,49 +14,50 @@ namespace Starboard.ViewModel
     using Starboard.Model;
     using Starboard.MVVM;
 
-    /// <summary> ViewModel controlling all the information necessary for databinding the scoreboards. </summary>
+    /// <summary>
+    /// ViewModel controlling all the information necessary for databinding the scoreboards.
+    /// </summary>
     public class ScoreboardControlViewModel : ObservableObject
     {
-        /// <summary> Holds our first player, which is initialized on creation. </summary>
-        private readonly Player player1 = new Player();
+        #region Constants and Fields
 
-        /// <summary> Holds our second player, which is initialized on creation. </summary>
-        private readonly Player player2 = new Player();
-
-        /// <summary> Contains the array of TimedText objects defining the subbar text. </summary>
-        private readonly ObservableCollection<TimedText> subbarText = new ObservableCollection<TimedText>();
-
-        /// <summary> Contains the array of TimedText objects defining the announcement text. </summary>
+        /// <summary>
+        ///   Contains the array of TimedText objects defining the announcement text.
+        /// </summary>
         private readonly ObservableCollection<TimedText> announcementText = new ObservableCollection<TimedText>();
 
-        /// <summary> Backing property for the IsAnnouncementShowing property. </summary>
+        /// <summary>
+        ///   Holds our first player, which is initialized on creation.
+        /// </summary>
+        private readonly Player player1 = new Player();
+
+        /// <summary>
+        ///   Holds our second player, which is initialized on creation.
+        /// </summary>
+        private readonly Player player2 = new Player();
+
+        /// <summary>
+        ///   Contains the array of TimedText objects defining the subbar text.
+        /// </summary>
+        private readonly ObservableCollection<TimedText> subbarText = new ObservableCollection<TimedText>();
+
+        /// <summary>
+        ///   Backing property for the IsAnnouncementShowing property.
+        /// </summary>
         private bool isAnnouncementShowing;
 
-        /// <summary> Backing property for the isSubbarShowing property. </summary>
+        /// <summary>
+        ///   Backing property for the isSubbarShowing property.
+        /// </summary>
         private bool isSubbarShowing;
 
-        /// <summary> Gets the first player's information. </summary>
-        public Player Player1
-        {
-            get { return this.player1; }
-        }
+        #endregion
 
-        /// <summary> Gets the second player's information. </summary>
-        public Player Player2
-        {
-            get { return this.player2; }
-        }
+        #region Public Properties
 
-        /// <summary> Gets the array containing the subbar text fields. </summary>
-        public ObservableCollection<TimedText> SubbarText
-        {
-            get
-            {
-                return this.subbarText;
-            }
-        }
-
-        /// <summary> Gets the array containing the announcement text fields. </summary>
+        /// <summary>
+        ///   Gets the array containing the announcement text fields.
+        /// </summary>
         public ObservableCollection<TimedText> AnnouncementText
         {
             get
@@ -65,7 +66,9 @@ namespace Starboard.ViewModel
             }
         }
 
-        /// <summary> Gets or sets a value indicating whether the announcement bar is showing. </summary>
+        /// <summary>
+        ///   Gets or sets a value indicating whether the announcement bar is showing.
+        /// </summary>
         public bool IsAnnouncementShowing
         {
             get
@@ -76,11 +79,13 @@ namespace Starboard.ViewModel
             set
             {
                 this.isAnnouncementShowing = value;
-                RaisePropertyChanged("IsAnnouncementShowing");
+                this.RaisePropertyChanged("IsAnnouncementShowing");
             }
         }
 
-        /// <summary> Gets or sets a value indicating whether the subbar is showing. </summary>
+        /// <summary>
+        ///   Gets or sets a value indicating whether the subbar is showing.
+        /// </summary>
         public bool IsSubbarShowing
         {
             get
@@ -91,8 +96,43 @@ namespace Starboard.ViewModel
             set
             {
                 this.isSubbarShowing = value;
-                RaisePropertyChanged("IsSubbarShowing");
+                this.RaisePropertyChanged("IsSubbarShowing");
             }
         }
+
+        /// <summary>
+        ///   Gets the first player's information.
+        /// </summary>
+        public Player Player1
+        {
+            get
+            {
+                return this.player1;
+            }
+        }
+
+        /// <summary>
+        ///   Gets the second player's information.
+        /// </summary>
+        public Player Player2
+        {
+            get
+            {
+                return this.player2;
+            }
+        }
+
+        /// <summary>
+        ///   Gets the array containing the subbar text fields.
+        /// </summary>
+        public ObservableCollection<TimedText> SubbarText
+        {
+            get
+            {
+                return this.subbarText;
+            }
+        }
+
+        #endregion
     }
 }
