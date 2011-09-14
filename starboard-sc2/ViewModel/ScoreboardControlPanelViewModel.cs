f918649622f2611f0b20989687e26e23a03aff84
Will Eddins <eddinsw@gmail.com>
2011-09-14 01:22:03 -0400

    Updated documentation

--- starboard-sc2/ViewModel/ScoreboardControlPanelViewModel.cs
+++ starboard-sc2/ViewModel/ScoreboardControlPanelViewModel.cs
@@ -1,23 +1,58 @@
-﻿namespace Starboard.ViewModel
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ScoreboardControlPanelViewModel.cs" company="Starboard">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   View model for the main scoreboard control view, which allows the user to
+//   change the player information, show the scoreboard, and all other critical updates.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starboard.ViewModel
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Windows.Input;
 
     using Starboard.Model;
     using Starboard.MVVM;
 
+    /// <summary>
+    /// The scoreboard control panel view model.
+    /// </summary>
     public class ScoreboardControlPanelViewModel : ObservableObject
     {
+        #region Constants and Fields
+
+        /// <summary> Reference to the active scoreboard view model. </summary>
+        private readonly ScoreboardControlViewModel scoreboard;
+
+        /// <summary> The backing field for the swap players command. </summary>
+        private ICommand swapPlayersCommand;
+
+        /// <summary> The backing field for the toggle announcement command. </summary>
+        private ICommand toggleAnnouncementCommand;
+
+        /// <summary> The backing field for the toggle scoreboard visible command. </summary>
+        private ICommand toggleScoreboardVisibleCommand;
+
+        /// <summary> The backing field for the toggle subbar command. </summary>
+        private ICommand toggleSubbarCommand;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary> Initializes a new instance of the <see cref="ScoreboardControlPanelViewModel"/> class. </summary>
+        /// <param name="scoreboard"> The scoreboard view model. </param>
         public ScoreboardControlPanelViewModel(ScoreboardControlViewModel scoreboard)
         {
             this.scoreboard = scoreboard;
         }
 
-        private ScoreboardControlViewModel scoreboard;
+        #endregion
 
+        #region Public Properties
+
+        /// <summary> Gets the active scoreboard view model. </summary>
         public ScoreboardControlViewModel Scoreboard
         {
             get
@@ -26,39 +61,7 @@
             }
         }
 
-        private ICommand toggleScoreboardVisibleCommand;
-        public ICommand ToggleScoreboardVisibleCommand
-        {
-            get
-            {
-                return this.toggleScoreboardVisibleCommand ?? (this.toggleScoreboardVisibleCommand = new RelayCommand(this.ToggleScoreboardVisible));
-            }
-        }
-
-        private ICommand toggleAnnouncementCommand;
-        public ICommand ToggleAnnouncementCommand
-        {
-            get
-            {
-                return this.toggleAnnouncementCommand ?? (this.toggleAnnouncementCommand = new RelayCommand(this.ToggleAnnouncement));
-            }
-        }
-
-        public void ToggleAnnouncement()
-        {
-            this.Scoreboard.IsAnnouncementShowing = !this.Scoreboard.IsAnnouncementShowing;
-        }
-
-        private ICommand toggleSubbarCommand;
-        public ICommand ToggleSubbarCommand
-        {
-            get
-            {
-                return this.toggleSubbarCommand ?? (this.toggleSubbarCommand = new RelayCommand(this.ToggleSubbar));
-            }
-        }
-
-        private ICommand swapPlayersCommand;
+        /// <summary> Gets a command to swap the players on the scoreboard. </summary>
         public ICommand SwapPlayersCommand
         {
             get
@@ -67,6 +70,40 @@
             }
         }
 
+        /// <summary> Gets a command to toggle the announcement display. </summary>
+        public ICommand ToggleAnnouncementCommand
+        {
+            get
+            {
+                return this.toggleAnnouncementCommand
+                       ?? (this.toggleAnnouncementCommand = new RelayCommand(this.ToggleAnnouncement));
+            }
+        }
+
+        /// <summary> Gets a command to toggle whether the scoreboard is visible. </summary>
+        public ICommand ToggleScoreboardVisibleCommand
+        {
+            get
+            {
+                return this.toggleScoreboardVisibleCommand
+                       ?? (this.toggleScoreboardVisibleCommand = new RelayCommand(this.ToggleScoreboardVisible));
+            }
+        }
+
+        /// <summary> Gets a command to toggle whether the subbar is visible. </summary>
+        public ICommand ToggleSubbarCommand
+        {
+            get
+            {
+                return this.toggleSubbarCommand ?? (this.toggleSubbarCommand = new RelayCommand(this.ToggleSubbar));
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary> Swaps the position of the two players, flipping left and right sides. </summary>
         public void SwapPlayers()
         {
             var player2 = (Player)this.Scoreboard.Player1.Clone();
@@ -83,15 +120,17 @@
             this.Scoreboard.Player2.Race = player2.Race;
         }
 
-        public void ToggleSubbar()
+        /// <summary> Toggles the visibility of the announcement board. </summary>
+        public void ToggleAnnouncement()
         {
-            this.Scoreboard.IsSubbarShowing = !this.Scoreboard.IsSubbarShowing;
+            this.Scoreboard.IsAnnouncementShowing = !this.Scoreboard.IsAnnouncementShowing;
         }
 
+        /// <summary> Toggles the visibility of the scoreboard. </summary>
         public void ToggleScoreboardVisible()
         {
             var display = MainWindowViewModel.DisplayWindow;
-            
+
             if (display.IsVisible)
             {
                 display.Hide();
@@ -102,5 +141,12 @@
             }
         }
 
+        /// <summary> Toggles whether the subbar is visible. </summary>
+        public void ToggleSubbar()
+        {
+            this.Scoreboard.IsSubbarShowing = !this.Scoreboard.IsSubbarShowing;
+        }
+
+        #endregion
     }
 }
