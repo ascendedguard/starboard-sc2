a7553b5bd5bfc537999bee22f61a70a6847cf5a0
Will Eddins <eddinsw@gmail.com>
2011-10-09 16:11:09 -0400

    Updated version to 1.3.2.* for release

--- starboard-sc2/Properties/AssemblyInfo.cs
+++ starboard-sc2/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.3.1.*")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.*")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
